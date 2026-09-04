ad2f1cd80cbea6cf529ea7737d3083d9fadca15f
IsabBosDev <isabosdev@gmail.com>
2026-09-04 15:33:43 +0100

    feat(backend): implement product controller and http test endpoints

---
+++ Backend/InventoryWebApi/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using InventoryWebApi.Business;
+using InventoryWebApi.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(ProductBusiness _service) : ControllerBase
+    {
+        // POST: api/product - Create a new product
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductRequest req)
+        {
+            var result = await _service.CreateAsync(req);
+            return Ok(result);
+        }
+
+        // GET: api/product?searchTerm=xyz - Retrieve all products with optional filter
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] string? searchTerm)
+        {
+            var result = await _service.GetAllAsync(searchTerm);
+            return Ok(result);
+        }
+    }
+}
