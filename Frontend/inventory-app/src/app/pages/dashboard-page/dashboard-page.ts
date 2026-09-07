87a3d20c95025540842eb2d73a6ce502e0661fc8
IsabBosDev <isabosdev@gmail.com>
2026-09-07 19:02:59 +0100

    feat(frontend): implement dashboard page with chart.js line charts and low stock alerts

--- Frontend/inventory-app/src/app/pages/dashboard-page/dashboard-page.ts
+++ Frontend/inventory-app/src/app/pages/dashboard-page/dashboard-page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, signal, ViewChild } from '@angular/core';
+import { InventoryService } from '../../services/inventory-service';
+import { GetProductReportResponse } from '../../interfaces/get-product-report-response';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   imports: [],
@@ -6,4 +10,65 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-page.css',
   templateUrl: './dashboard-page.html',
 })
-export class DashboardPage {}
+export class DashboardPage implements AfterViewInit {
+  // Reference to the canvas element in the template
+  @ViewChild('movementsChart') private movementsChart!: ElementRef<HTMLCanvasElement>;
+
+  private chartMovement!:Chart;
+  
+  // HTTP service injection
+  private inventoryService = inject(InventoryService);
+
+  // Signal holding low stock products list
+  protected products = signal<GetProductReportResponse[]>([]);
+
+  protected showChartMovement(labels:string[], entries:number[], exits:number[]){
+    this.chartMovement = new Chart(this.movementsChart.nativeElement, {
+      type: 'line',
+      data: {
+        labels,
+        datasets: [
+          {
+            label: 'Entries',
+            data: entries,
+            borderColor: '#1c5aaf',
+          },
+          {
+            label: 'Exits',
+            data: exits,
+            borderColor: '#14643f',
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: true,
+          }
+        }
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.inventoryService.getMovementReport().subscribe({
+      next: resp => {
+        if(resp.isSuccess) {
+          const labels = resp.data.map(x => x.day)
+          const entries = resp.data.map(x => x.entries)
+          const exits = resp.data.map(x => x.exits)
+          this.showChartMovement(labels, entries, exits)
+        }
+      }
+    })
+     this.inventoryService.getProductReport().subscribe({
+      next: resp => {
+        if(resp.isSuccess) {
+          this.products.set(resp.data)
+        }
+      }
+    })
+  }
+}   
